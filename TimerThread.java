894d504442e1a9e066693ee5b2ddd1fd56a4d027
Cipro03 <93771682+Cipro2003@users.noreply.github.com>
2022-05-06 10:43:11 +0000

    second commit

--- TimerThread.java
+++ TimerThread.java
@@ -1,7 +1,21 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class TimerThread extends Thread {
+  private int tempo;
+  public TimerThread(int t) {
+    this.tempo=t;
+  }
   
   @Override
   public void run(){
-    
+  for(int i = tempo; i>0; i--){
+    try{
+        System.out.println(i);
+        sleep(1000);
+    } catch (InterruptedException ex) {
+      Logger.getLogger(TimerThread.class.getName()).log(Level.SEVERE, null, ex);
+      } 
+    }
   }
 }
